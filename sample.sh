4e6f0aa8d8ecf97356c2eaf36fc4c1637232424c
root <root@ip-172-31-50-28.ec2.internal>
2021-02-19 06:42:53 +0000

    second commit

--- sample.sh
+++ sample.sh
@@ -5,7 +5,7 @@ PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
 TYPE=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
 ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
 
-echo "$PH"
+echo "The public host name is $PH"
 echo "$PIP"
 echo "$PSG"
 echo "$TYPE"
